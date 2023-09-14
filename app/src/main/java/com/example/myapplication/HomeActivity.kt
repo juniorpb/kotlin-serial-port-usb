01e6759fe89f51f168ca95962a5d6e4b16fa0eb9
juniorpb <junior.cgjr@gmail.com>
2023-09-13 21:56:20 -0300

    feat: create aniaml view

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,11 +1,54 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityHomeBinding
+import com.google.android.material.snackbar.Snackbar
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnEntradaAnimal.setOnClickListener {
+            val intent = Intent(this, EntradaAnimalActivity::class.java)
+            startActivity(intent)
+        }
+
+        val CREATE_ANIMAL_RESPONSE = intent.getStringExtra("CREATE_ANIMAL_RESPONSE")
+
+        if (CREATE_ANIMAL_RESPONSE == "SUCCESS") {
+            showToastSuccess()
+        } else if(CREATE_ANIMAL_RESPONSE == "ERROR") {
+            showToastError()
+        }
+
+    }
+
+    private fun showToastSuccess() {
+        val mensagem = "Sucesso ao criar animal!"
+
+        val snackbar = Snackbar.make(binding.root, mensagem, Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(resources.getColor(android.R.color.holo_green_dark))
+        snackbar.show()
+    }
+
+    private fun showToastError() {
+        val mensagem = "Erro ao criar animal"
+
+        val snackbar = Snackbar.make(binding.root, mensagem, Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(resources.getColor(android.R.color.holo_red_dark))
+        snackbar.show()
     }
 }
